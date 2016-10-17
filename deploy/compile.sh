5e4366d22fc2a966d10b16a347e565624794e46f
Zhaofeng Li <hello@zhaofeng.li>
2016-10-17 00:04:55 -0700

    compile.sh: Where did the directory come from :(

--- deploy/compile.sh
+++ deploy/compile.sh
@@ -7,6 +7,7 @@ cp -r `find . -maxdepth 1 -! \( -name "out" -o -name ".*" -o -name "deploy" \)` 
 # Delete unused files
 rm -f out/.gitignore
 rm -rf out/deploy
+rm -rf out/out
 
 # Add bower dependencies
 cd out
