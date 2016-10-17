d037fe5cc2aaefd89a0c7cee6d1caa2da319ed68
Zhaofeng Li <hello@zhaofeng.li>
2016-10-16 23:15:44 -0700

    deploy/deploy.sh: Wrong path :(

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -7,7 +7,7 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 function doCompile {
-  ./compile.sh
+  ./deploy/compile.sh
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
