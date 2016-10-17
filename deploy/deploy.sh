4feab325e6ddd51a2bbb7511a4aaa4e0c29e125d
Zhaofeng Li <hello@zhaofeng.li>
2016-10-16 23:27:54 -0700

    deploy.sh: Path

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -57,7 +57,7 @@ ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy/deploy_key.enc -out ../deploy/deploy_key -d
-chmod 600 deploy_key
+chmod 600 ../deploy/deploy_key
 eval `ssh-agent -s`
 ssh-add ../deploy/deploy_key
 
